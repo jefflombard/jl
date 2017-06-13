360a01cf7773ee4708d90f31ea7df411dd679673
Jeff Lombard <jeffl>
2017-06-12 17:58:11 -0700

    Styling good

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Skill from './Skills/Skill';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.setState({
+      searchTerm: null
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Skill name="javascript"/>
+        <Skill name="react"/>
+        <Skill name="jest"/>
+        <Skill name="functional programming"/>
       </div>
     );
   }
---
+++ src/Skills/SkillContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Skill.css';
+
+class SkillsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Skill/>
+      </div>
+    )
+  }
+}
+
+export default SkillsContainer;
